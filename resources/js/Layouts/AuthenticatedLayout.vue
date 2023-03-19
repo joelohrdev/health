53b3fb3b3bb7604bef85c8208961fd2a0316b929
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2023-03-19 15:26:49 -0500

    Added weight model

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,6 +32,9 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+                                <NavLink :href="route('weight')" :active="route().current('weight')">
+                                    Weight
+                                </NavLink>
                             </div>
                         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/weight', function () {
+    return Inertia::render('Weight');
+})->middleware(['auth', 'verified'])->name('weight');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
